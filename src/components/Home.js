0a1fcdded7a7d5d40ce9a5c2be582d8d4a1e1df2
AcoGlezRojo <acoidangonzalezrojo@gmail.com>
2023-01-01 21:46:20 +0000

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>
+                Hola Mundo
+            </h1>
+        </div>
+    )
+}
+
